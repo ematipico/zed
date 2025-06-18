b42b6e866ccee087c0e69d7c62c346dc8b3e0fff
Julia Ryan <p1n3appl3@users.noreply.github.com>
2025-06-18 03:04:38 -0700

    WIP

--- crates/debugger_ui/src/attach_modal.rs
+++ crates/debugger_ui/src/attach_modal.rs
@@ -206,6 +206,7 @@ impl PickerDelegate for AttachModalDelegate {
     }
 
     fn confirm(&mut self, _: bool, window: &mut Window, cx: &mut Context<Picker<Self>>) {
+        // TODO: use secondary confirm to save to debug.json like with new_process
         let candidate = self
             .matches
             .get(self.selected_index())
